10f40a63e8c58547d3077dc2adcfde80296871cb
0soliman0 <solimanelaraby81@gmail.com>
2023-03-12 16:40:55 +0200

    current screen sizes

--- lib/sections/screen_size_note.dart
+++ lib/sections/screen_size_note.dart
@@ -5,12 +5,12 @@ class ScreenSizeNote extends StatelessWidget {
   final double? width, height;
   @override
   Widget build(BuildContext context) {
-    return height! > 360 || width! > 720
+    return height! != 360 || width! != 710
         ? Container(
             alignment: Alignment.center,
             color: const Color(0xFFebbd34),
             child: const Text(
-              "The screen size is not appropriate. \n 360 x 720 or 360 x 710 is required sizes for now. \n The Design will be responsive in next time. \n The project is under construction. \n Thanks",
+              "The screen size is not appropriate. \n 360 x 710 is required sizes for now. \n The Design will be responsive in next time. \n The project is under construction. \n Thanks",
               textAlign: TextAlign.center,
               style: TextStyle(height: 1.3, fontWeight: FontWeight.bold),
             ))
